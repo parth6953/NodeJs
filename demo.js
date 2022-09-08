896917d498edb4fd446de7b5f5751ce5483e811e
parth6953 <92851579+parth6953@users.noreply.github.com>
2022-09-08 11:29:36 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+console.log("Welcome to Node JS");
+var a=10;
+var b=20;
+var c=a+b;
+console.log("A value is "+a+"B value is"+b);
+console.log("Sum is "+c)
+// ES6
+console.log(`A value is ${a} B value is${b}`);
+
+
+if(c==30)
+{
+console.log("Sum is 30.");
+}else{
+console.log("Else")
+}
